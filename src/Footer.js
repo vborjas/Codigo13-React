0001e77d2d45860c1fea91da1c542b57d56f74ac
Victor Borjas <victorborjas2507@gmail.com>
2022-03-15 11:55:22 -0500

    react inicio

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div>
+        <footer>
+         
+            <ul>
+              <li>logo</li>
+              <li>redes</li>
+             
+            </ul>
+          
+        </footer>
+      </div>
+    );
+  };
+  
+  export default Footer;
